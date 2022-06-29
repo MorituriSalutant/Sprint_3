e35692bc4c925ca58ead553521fc202b1699c372
Alexandr Perebeynos <aperebejnos2014@yandex.ru>
2022-06-29 20:02:32 +0300

    Убрал лишние данные в тесте с параметризацией, добавил генератор данных

--- src/test/java/OrderCreateTest.java
+++ src/test/java/OrderCreateTest.java
@@ -1,4 +1,5 @@
 import api.client.OrderApiClient;
+import com.github.javafaker.Faker;
 import io.qameta.allure.Description;
 import io.qameta.allure.Feature;
 import io.qameta.allure.junit4.DisplayName;
@@ -10,48 +11,35 @@ import org.junit.runners.Parameterized;
 import pojo.order.OrderCreateJson;
 
 import java.util.List;
+import java.util.Locale;
 
 import static org.hamcrest.Matchers.notNullValue;
 
 @Feature("Создание заказов")
 @RunWith(Parameterized.class)
 public class OrderCreateTest {
-    public String firstName;
-    public String lastName;
-    public String address;
-    public int metroStation;
-    public String phone;
-    public int rentTime;
-    public String deliveryDate;
-    public String comment;
-    public List<String> color;
+
+    private final List<String> color;
     OrderApiClient orderApiClient;
     OrderCreateJson orderCreateJson;
 
-    public OrderCreateTest(String firstName, String lastName, String address, int metroStation, String phone, int rentTime, String deliveryDate, String comment, List<String> color) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.address = address;
-        this.metroStation = metroStation;
-        this.phone = phone;
-        this.rentTime = rentTime;
-        this.deliveryDate = deliveryDate;
-        this.comment = comment;
+    public OrderCreateTest(List<String> color) {
         this.color = color;
     }
 
     @Before
     public void setUp() {
         orderApiClient = new OrderApiClient();
+        Faker faker = new Faker(new Locale("ru"));
         orderCreateJson = new OrderCreateJson(
-                firstName,
-                lastName,
-                address,
-                metroStation,
-                phone,
-                rentTime,
-                deliveryDate,
-                comment,
+                faker.name().firstName(),
+                faker.name().lastName(),
+                faker.address().fullAddress(),
+                faker.number().randomDigitNotZero(),
+                faker.phoneNumber().cellPhone(),
+                faker.number().randomDigitNotZero(),
+                faker.business().creditCardExpiry(),
+                faker.elderScrolls().quote(),
                 color
         );
     }
@@ -59,46 +47,9 @@ public class OrderCreateTest {
     @Parameterized.Parameters
     public static Object[][] getData() {
         return new Object[][]{
-                {
-                        "Alexanrd",
-                        "Chepukhov",
-                        "ул. Пушкина д.Колотушкина",
-                        4,
-                        "+7 906 90 64 321",
-                        5,
-                        "2020-06-06",
-                        "comment",
-                        List.of("BLACK")},
-                {
-                        "Oleg",
-                        "Chepukhov",
-                        "ул. Пушкина д.Колотушкина",
-                        4,
-                        "+7 906 90 64 321",
-                        5,
-                        "2020-06-06",
-                        "comment",
-                        List.of("GREY")},
-                {
-                        "Katya",
-                        "Chepukhov",
-                        "ул. Пушкина д.Колотушкина",
-                        4,
-                        "+7 906 90 64 321",
-                        5,
-                        "2020-06-06",
-                        "comment",
-                        List.of("GREY", "BLACK")},
-                {
-                        "Dima",
-                        "Malakhov",
-                        "ул. Пушкина д.Колотушкина",
-                        4,
-                        "+7 906 90 64 321",
-                        5,
-                        "2020-06-06",
-                        "comment",
-                        null}
+                {List.of("GREY")},
+                {List.of("GREY", "BLACK")},
+                {null}
         };
     }
 
